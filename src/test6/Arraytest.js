a2e0b3ce6604fb91479c48ceaf222f335d1b1ba6
Minseok Kim <comko123@naver.com>
2022-10-16 02:06:21 +0900

    capstone refactoring

--- src/test6/Arraytest.js
+++ src/test6/Arraytest.js
@@ -1,5 +1,7 @@
-
+import axios from "axios"
+import { useState } from "react"
 import {useNavigate } from "react-router-dom"
+
 const current_date = new Date()
 const join_month = current_date.getMonth()+1
 const getDate = current_date.getFullYear()+"년 "
@@ -39,54 +41,74 @@ const selectUserData = {
     성별:["남자","여자"],
     신장: ["156 ~ 160cm","160 ~ 165cm","165 ~ 170cm","170 ~ 175cm","175이상"],
     체중: ["45 ~ 55kg","56 ~ 65kg","66 ~ 75kg","76 ~ 85kg","85kg 이상"],
-     스타일:["빈티지","댄디","캐주얼","놈코어","스트릿"]
+    스타일:["빈티지","댄디","캐주얼","놈코어","스트릿"]
   }
  const userClientInput = {id:[],email:[],password:[],password2:[],nickname:[]}
  const userSelect = {성별:[],연령:[],신장:[],체중:[],스타일:[]}
 const basicSetting = Object.keys(selectUserData)
 const userSetting = Object.keys(inputTypeAndPalcehorder)
-const signUpFunction = async(call) => {
+const styleObject = Object.keys(selectUserCheck)
+const signUpFunction = async(result,setResult,navigate) => {
   const {id,password,password2,email,nickname} = userClientInput
   const {성별,연령,신장,체중,스타일} = userSelect
-const userInfo = new signUpData(id[0],password[0],password2[0],email[0],nickname[0],성별[0],연령[0],신장[0],체중[0],스타일[0])
-try{const sign = await fetch(`/join`, {
-  method : "post",
-  body : JSON.stringify(userInfo),
-  headers : {"Content-Type" : "application/json"}})
-if(sign.ok){alert("Completed for Sign Up :) ")
-call('/')}}
-catch(e){console.log(e); alert("Fail To Sign Up :(")}
- console.log(userInfo)
+const userInfo = new signUpData(id[0],password[0],password2[0],email[0],nickname[0],성별[0],연령[0],신장[0],체중[0],스타일)
+try{  
+setResult(await(await axios.post('/join',userInfo)).data)
+return result&&typeof(result)==="object"?alert(result.errorMessag):typeof(result)==="string"?(alert(result),navigate(-1)):null
+}
+catch(e){console.log(e)}
+console.log(userInfo)
 }
 
 const userInFormationRadio = (...rest) => {
 return(<div key={rest[3]}><h3 style = {{"marginBottom":"0px"}}>{rest[1]}</h3>
 {rest[0].map((childInformation,index)=>{
 return (<div key = {index}>{childInformation}
-<input type ="radio"name={rest[1]} value = {`${childInformation}`} onClick ={(e)=>
+<input type ="radio"name={rest[1]} value = {
+  childInformation==="남성"||childInformation==="여성"?childInformation:index
+} onClick ={(e)=>
   {
-    rest[2].push(e.target.value)  
-    if(rest[2][0]===rest[2][1]){
-      rest[2].splice(rest[2].indexOf(e.target.value),1)}
-   console.log(rest[2])//수정 필요
-}}/> <br/></div>)})}</div>)}
+    if(!!!rest[2].length){rest[2].push(e.target.value)}
+      else{
+      rest[2].splice(rest[2].indexOf(e.target.value),1)
+      rest[2].push(e.target.value)
+    }
+}}/> <br/></div>)})}</div>)
+}
+
+const userInFormationCheckBox = (...rest) =>{
+  return(<div key = {rest[0]}>
+  <h3>{rest[0]}</h3>
+  {rest[1].map((clothing,index)=>{
+  return (<div key = {index}>{clothing}<input type ="checkbox" 
+  value = {rest[0]+`/${clothing}`} onClick ={e=>
+    {if(e.target.checked===true){
+     rest[2].push(e.target.value)  
+    rest[2].filter((element,index)=>rest[2].indexOf(element)===index)}
+      else{rest[2].splice(rest[2].indexOf(e.target.value),1)}
+  }}/> <br/></div>)})}</div>)
+}
 
 const userInFormationInput = (...rest) => {
   return(
   <div key={rest[3]}><input type ={rest[0]} placeholder={rest[1]} onBlur={(e)=>{
   rest[2]&& rest[2].push(e.target.value)
   rest[2]&& rest[2].splice(0,rest[2].length-1)
-  }}/><br/> <br/></div>)}
+  }}/><br/> <br/></div>)
+}
 
-export default function Sign_Up (){
+  
+export default function Arraytest (){
   const trans = useNavigate()
+  const [result,setResult] = useState(null)
 return(<>
   <h1>회원가입</h1>
   <form onSubmit={e=>e.preventDefault()}>
   {userSetting.map((item,index)=>userInFormationInput(inputTypeAndPalcehorder[userSetting[index]][0],item,userClientInput[item],index))}
   {basicSetting.map((item,index)=>userInFormationRadio(selectUserData[item],item,userSelect[item],index))}
+  {styleObject.map((item,index)=>userInFormationCheckBox(item,selectUserCheck[item],userSelect[styleObject[0]],index))}
   <br/>
- <input type="submit" value="가입"onClick={()=>signUpFunction(trans)}/>   
+ <input type="submit" value="가입"onClick={()=>{signUpFunction(result,setResult,trans)}}/>   
   <br/> <br/>
  <input type="button" value="홈페이지"onClick={()=>trans('/')}/>  
   </form></>)}
