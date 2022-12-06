533eaea14a7d88574fe6749859e2ed9987c5ca28
Minseok Kim <comko123@naver.com>
2022-12-07 00:02:49 +0900

    .

--- src/store.js
+++ src/store.js
@@ -4,49 +4,36 @@ import { configureStore ,createSlice} from "@reduxjs/toolkit"
 const data = createSlice({
 name : "data",
 initialState : {a:30,b:20},
-reducers:{
-    inputData(state,want){return state = want.payload}
-}})
+reducers:{inputData(state,want){return state = want.payload}}})
 
 const stock = createSlice({
     name:"stock",
     initialState:[1,2,3],
-    reducers:{
-        stack(state,arraying){state.push(arraying.payload)}
-    }})
+    reducers:{stack(state,arraying){state.push(arraying.payload)}}})
 
 const registration = createSlice({
     name:"registration",
     initialState:{spring:[],summer:[],fall:[],winter:[]},
     reducers:{
         addStyle(state,action){
-      state[action.payload.list].push(action.payload.value)
-        },
+        state[action.payload.list].push(action.payload.value)},
         deduplicationStyle(state,action){
-            state[action.payload.list].filter((element,index)=>state[action.payload.list].indexOf(element)===index)
-        }
-        ,
+        state[action.payload.list].filter((element,index)=>state[action.payload.list].indexOf(element)===index)},
         removeStyle(state,action){
-            state[action.payload.list].splice(state[action.payload.list].indexOf(action.payload.value),1)
-        }
-    }})
+        state[action.payload.list].splice(state[action.payload.list].indexOf(action.payload.value),1)
+        }}})
 
 const modify = createSlice({
     name:"modify",
     initialState:{spring:[],summer:[],fall:[],winter:[],style:[]},
     reducers:{
-        addStyle1(state,action){
-            state[action.payload.list].push(action.payload.value)
-              },
-              deduplicationStyle1(state,action){
-                  state[action.payload.list].filter((element,index)=>state[action.payload.list].indexOf(element)===index)
-              }
-              ,
-              removeStyle1(state,action){
-                  state[action.payload.list].splice(state[action.payload.list].indexOf(action.payload.value),1)
-              }
-    }
-})
+            addStyle1(state,action){
+            state[action.payload.list].push(action.payload.value)},
+            deduplicationStyle1(state,action){
+            state[action.payload.list].filter((element,index)=>state[action.payload.list].indexOf(element)===index)},
+            removeStyle1(state,action){
+            state[action.payload.list].splice(state[action.payload.list].indexOf(action.payload.value),1)
+            }}})
 export const{inputData} = data.actions
 export const{stack} = stock.actions
 export const{addStyle,removeStyle,deduplicationStyle} = registration.actions
@@ -55,5 +42,4 @@ export default configureStore({
     reducer:{
    orUser : data.reducer,
    orStock : stock.reducer,
-   addStyleList : registration.reducer
-    }})
+   addStyleList : registration.reducer}})
