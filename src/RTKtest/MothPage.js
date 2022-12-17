35ba5f0af189144ae7ef580befb670e7e55d15b6
Minseok Kim <comko123@naver.com>
2022-12-18 00:34:17 +0900

    .

--- src/RTKtest/MothPage.js
+++ src/RTKtest/MothPage.js
@@ -2,7 +2,7 @@
 import { useSelector } from "react-redux"
 const MothPage = () => {
     const selector = useSelector(G=>G)
-    const text = selector.orUser;
+    const text = selector.orUser
     return(<div style = {{"textAlign" : "center"}} >
     <h1 style = {{"backgroundColor":"tomato"}}>Moth Page</h1> 
     <br/>
