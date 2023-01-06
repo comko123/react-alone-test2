2e437d699df8dfc6ef2caa0b3a3bd9f59be46965
Minseok Kim <comko123@naver.com>
2023-01-06 19:27:32 +0900

    .

--- src/test9/Board.js
+++ src/test9/Board.js
@@ -1,8 +1,7 @@
 const board = () =>{
         return (<>
         <h1>게시판</h1>
-        <br/>
-        <span>
+        <br/><span>
         <span>더 살펴보기</span>
         <span>글쓰기</span>
         </span></>)}
