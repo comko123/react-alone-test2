674892d00861e5d737ef9d41d083ed8939adad11
Minseok Kim <comko123@naver.com>
2023-01-08 15:09:06 +0900

    .

--- src/test9/Comment.js
+++ src/test9/Comment.js
@@ -3,13 +3,9 @@ return(<>
 <ul>
     <><li><h4>1</h4>  <span>삭제하기</span><span>수정하기</span></li></>
     <><li><h4>2</h4> <span>삭제하기</span><span>수정하기</span></li></>
-</ul>
-<hr/>
+</ul><hr/>
 <h4>댓글 작성하기</h4>
-<form onSubmit={e=>{e.preventDefault()
-}}>
-    <textarea placeholder="댓글을 작성해 보세요." rows="5" cols="80"/> 
-    <input type = "submit" value = "작성 하기"/>
-</form>
-</>)}
+<form onSubmit={e=>{e.preventDefault()}}>
+<textarea placeholder="댓글을 작성해 보세요." rows="5" cols="80"/> 
+<input type = "submit" value = "작성 하기"/></form></>)}
 export default Comment
